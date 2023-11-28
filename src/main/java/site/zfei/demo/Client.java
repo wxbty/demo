fd2df972a3d19da808b0f13687a6ba5abf539d58
林尧 <wangxiaobin@cai-inc.com>
2023-11-28 17:12:49 +0800

    use con

--- src/main/java/site/zfei/demo/Client.java
+++ src/main/java/site/zfei/demo/Client.java
@@ -1,15 +1,16 @@
 package site.zfei.demo;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Client {
 
     public void addByMap() {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new ConcurrentHashMap<>();
         for (int i = 0; i < 10000; i++) {
             map.put("key" + i, i);
         }
+        System.out.println("use concurrentHashMap");
     }
 
 
