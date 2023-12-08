37a8531934b17e6c94013cb19b291f077279df72
林尧 <wangxiaobin@cai-inc.com>
2023-12-08 19:48:03 +0800

    v2

--- src/main/java/site/zfei/demo/Client.java
+++ src/main/java/site/zfei/demo/Client.java
@@ -7,7 +7,7 @@ public class Client {
 
     public void addByMap() {
         Map<String, Integer> map = new ConcurrentHashMap<>();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             map.put("key" + i, i);
             try {
                 Thread.sleep(1);
